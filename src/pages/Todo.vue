5c563191034ea354f2dc530d14c7f7ab77ccd9be
bartekcostam <bartekoffice1@gmail.com>
2020-02-02 21:35:37 +0000

    Delete- dialog box

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,45 +1,21 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <q-list>
-      <!--
-        Rendering a <label> tag (notice tag="label")
-        so QCheckboxes will respond to clicks on QItems to
-        change Toggle state.
-      -->
-
-      <q-item tag="label" v-ripple>
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done =! task.done"
+        :class="{ 'done bg-blue-1' : task.done}"
+        clickable
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="color" val="teal" color="teal" />
+          <q-checkbox v-model="task.done" class="no-pointer-events" color="primary" />
         </q-item-section>
         <q-item-section>
-          <q-item-label>Teal</q-item-label>
+          <q-item-label>{{ task.title}}</q-item-label>
         </q-item-section>
-      </q-item>
-
-      <q-item tag="label" v-ripple>
-        <q-item-section avatar>
-          <q-checkbox v-model="color" val="orange" color="orange" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Orange</q-item-label>
-          <q-item-label caption>With description</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-item tag="label" v-ripple>
-        <q-item-section avatar top>
-          <q-checkbox v-model="color" val="cyan" color="cyan" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Cyan</q-item-label>
-          <q-item-label caption>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </q-item-label>
+        <q-item-section v-if="task.done" side>
+          <q-btn @click.stop="deleteTask(index)" flat round dense color="primary" icon="delete" />
         </q-item-section>
       </q-item>
     </q-list>
@@ -47,5 +23,48 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Code",
+          done: false
+        },
+        {
+          title: "Eat",
+          done: true
+        },
+        {
+          title: "Sleep",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "Confirm",
+          message: "Are you sure you want to delete?",
+          cancel: true,
+          persistent: true
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+        });
+    }
+  }
+};
 </script>
+
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
