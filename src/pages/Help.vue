0204140b7d7c0f18bc6d2f78cdb10c82ec519c75
bartekcostam <bartekoffice1@gmail.com>
2020-02-03 09:12:54 +0000

    parsing code

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Help</h5>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur omnis nostrum ea est facere! Distinctio!</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur omnis nostrum ea est facere! Distinctio!</p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
+      omnis nostrum ea est facere! Distinctio!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
+      omnis nostrum ea est facere! Distinctio!
+    </p>
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
-
-sc
