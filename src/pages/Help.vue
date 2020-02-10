613526c70cf3506ec1cf50bde289f36b2c4f1c56
bartekcostam <bartekoffice1@gmail.com>
2020-02-10 23:06:42 +0000

    info

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,10 +1,7 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Help</h5>
-    <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
-      omnis nostrum ea est facere! Distinctio!
-    </p>
+    <p>This is help page..</p>
     <p>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
       omnis nostrum ea est facere! Distinctio!
@@ -13,5 +10,5 @@
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
